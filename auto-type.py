84841b85bd5049dbc59a59be2990351e7b70724e
Danteh <90870997+EsadTheDumbass@users.noreply.github.com>
2021-12-09 23:21:22 +0100

    A typing test auto-completion python script
    (Very surface level and not detailed, Only works on https://www.keyhero.com/free-typing-test/  And also for it to work you have to not scroll down the ad in the top.

---
+++ auto-type.py
@@ -0,0 +1,28 @@
+import time
+from PIL import ImageGrab
+import pytesseract
+import matplotlib.pyplot as mt
+import numpy as np
+import pyautogui
+import cv2
+import fileinput
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Users/ahmet/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
+
+
+def convertandtype():
+    time.sleep(5)
+    cap = ImageGrab.grab(bbox=(106, 294, 853, 515))
+    tesstr = pytesseract.image_to_string(
+        cv2.cvtColor(np.array(cap), cv2.COLOR_BGR2GRAY),
+        lang='eng')
+    print(tesstr)
+    script = open('text.txt', 'w')
+    script.write(tesstr)
+    script = open('text.txt', 'r')
+    for word in script:
+        if not word.isspace():
+            pyautogui.write(word, interval=0.08
+
+
+convertandtype()
