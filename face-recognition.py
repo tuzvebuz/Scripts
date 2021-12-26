adb3ab8bd8efff189b26646415dd03f31ed748ba
Esad Kocaturk <esadkocaturk.nl@gmail.com>
2021-12-26 21:37:03 +0100

    AI Image recognition

--- face-recognition.py
+++ face-recognition.py
@@ -1,7 +1,15 @@
-from imageai.Detection import ObjectDetection
 
-tarayici = ObjectDetection()
-tarayici.setModelTypeAsTinyYOLOv3()
-tarayici.setModelPath('C:/Users/ahmet/Downloads/yolo-tiny.h5')
-tarayici.loadModel()
-tarayici.detectObjectsFromImage(input_image="C:/Users/ahmet/Desktop/Scripts[Codedbyme]/images/bike.jpg", output_image_path="bikenew.jpg", minimum_percentage_probability=30)
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+detector = ObjectDetection()
+detector.setModelTypeAsTinyYOLOv3()
+detector.setModelPath(os.path.join(execution_path , "yolo-tiny.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "elmalar.jpg"), output_image_path=os.path.join(execution_path , "elmaolacaksahislar.jpg"), minimum_percentage_probability=30)
+
+for eachObject in detections:
+    print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"])
+    print("--------------------------------")
