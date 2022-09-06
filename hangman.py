944df5cf5bce08eaa8bba84e330a188991e75dda
Danteh <esadkocaturk.nl@gmail.com>
2022-09-06 23:07:25 +0200

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -15,23 +15,24 @@ guess = input('Please guess the word with either a letter or the full word.')
 positions = 0
 def display_board():
     print(hangman)
-def get_pos():
-    while positions < len(word):
-        positions = word.find(guess, positions)
-        if positions == -1:
-            break
-        print('Your guess has been found at index:', positions)
-        positions += 1 # +2 because len('ll') == 2
-        return positions
+#def get_pos():
+    
 def if_is(letter: str):
     if letter in word:
         return True
     else:
         return False
 if guess in word:
-    
-    print(positions)
+    pos = []
+    while positions < len(word):
+        positions = word.find(guess, positions)
+        if positions == -1:
+            break
+        print('Your guess has been found at index:', positions)
+        pos.append(positions)
+        positions += 1 # +2 because len('ll') == 2
+        
+    print(pos[0:])
     print(word)
-    
 
     print('Correct')
